08e9270f7d4166f92193664c4510f0473c672298
MoscowDev <mosesogomegbunam@gmail.com>
2026-01-28 13:37:34 +0100

    customer works

---
+++ src/main/java/com/moscow/restaurantapp/data/models/Customer.java
@@ -0,0 +1,10 @@
+package com.moscow.restaurantapp.data.models;
+
+public class Customer {
+    private String name;
+    private String id;
+    private String email;
+    private String phone;
+    private OrderStatus orderStatus;
+
+}
---
+++ src/main/java/com/moscow/restaurantapp/dtos/requests/CustomerRequest.java
@@ -0,0 +1,6 @@
+package com.moscow.restaurantapp.dtos.requests;
+
+public class CustomerRequest {
+    public String name;
+    public String phoneNumber;
+}
